4408af01c582833a5775051e5cf54009db3fa44c
Asker <kotsevasker0@gmail.com>
2023-06-28 22:27:24 +0300

    Initial Commit

---
+++ TopApp/TopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopAppApp.swift
+//  TopApp
+//
+//  Created by ASKER on 28.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
